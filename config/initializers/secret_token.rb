d7b2b1809ec12b01239973b7cbfef48d0e0d194d
Norm Euker <admin@njedesign.com>
2011-01-13 15:54:41 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myhelptoolbox2::Application.config.secret_token = 'a4a8ee4017fa0341f17ae14ba59eb867173cbfa381bd9c2589cdcc4c06682d74caa914d52bd76b8a4abbc3127914dc7eb05480b3532cc26380ccc20d1d9c0866'
